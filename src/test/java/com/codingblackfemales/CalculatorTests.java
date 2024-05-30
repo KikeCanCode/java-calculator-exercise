6cc7152f3b82f93fa6e3e77cd5a4bf964e6f9598
kikedare78@gmail.com <kikedare78@gmail.com>
2024-05-30 21:29:28 +0100

    added new branch

--- src/test/java/com/codingblackfemales/CalculatorTests.java
+++ src/test/java/com/codingblackfemales/CalculatorTests.java
@@ -23,4 +23,33 @@ public class CalculatorTests {
 
         assertEquals(-2, difference);
     }
+    @Test
+    @DisplayName("Multiply two positive numbers")
+    public void testMultiplyTwoPositiveNumbers() {
+        final Integer product = Calculator.multiply(3, 4);
+        assertEquals(12, product);
+    }
+
+    @Test
+    @DisplayName("Multiply a positive number and a negative number")
+    public void testMultiplyPositiveAndNegativeNumbers() {
+        final Integer product = Calculator.multiply(3, -4);
+        assertEquals(-12, product);
+    }
+
+
+    @Test 
+    @DisplayName ("Divide two positive numbers")
+    public void testDivideTwoPositiveNumbers() {
+        final Double result = Calculator.divide(6.0, 3.0);
+        assertEquals(2.0, result);
+
+    }
+
+    @Test
+    @DisplayName("Divide a number by itself")
+    public void testDivideByItself() {
+        final Double result = Calculator.divide(6.0, 6.0);
+        assertEquals(1.0, result);
+    }
 }
